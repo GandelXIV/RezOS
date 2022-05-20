c040f60beddf0d1bf5b7b3feda431b5a2ee96d49
Filip Chovanec <61948151+GandelXIV@users.noreply.github.com>
2022-05-20 09:03:21 +0200

    Update puts.asm

--- boot/real/io/puts.asm
+++ boot/real/io/puts.asm
@@ -1,28 +1,33 @@
+; print interrupt
 INT_VIDEO equ 0x10
 
+; string = (zero terminated char array)
+
+; put string 
 %macro real_puts 1
-pusha
-mov bx, %1
+pusha       ; push all registers so they can be retrieved later
+mov bx, %1  ; ld string pointer 
 call real_puts_start
-popa
+popa        ; retrieve registers
 %endmacro
 
+; put string on new line
 %macro real_putsln 1
 real_puts %1
-scroll
+scroll  ; defined in boot/real/io/nl.asm
 %endmacro
 
 real_puts_start:
-mov ah, 0x0E
+mov ah, 0x0E  ; config
 
 real_puts_loop:
-mov al, [bx] ; load new char
+mov al, [bx]  ; load new char
 
-cmp al, 0   ; check for string null-termination
+cmp al, 0     ; check for string null-termination
 je real_puts_end
 
-int INT_VIDEO   ; print current char
-inc bx  ; select next char
+int INT_VIDEO       ; print current char
+inc bx              ; select next char by incrementing the pointer
 jmp real_puts_loop  ; repeat
 
 real_puts_end:
