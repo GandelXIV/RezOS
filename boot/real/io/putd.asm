6487359b275114727fc0378d7a085d961a69f345
Romner <Roman@skotnica.com>
2022-05-24 14:59:34 +0200

    Fix putd.asm

--- boot/real/io/putd.asm
+++ boot/real/io/putd.asm
@@ -1,5 +1,6 @@
 N_TO_ASCII_DIFF equ 48  ; int(n + this = 
 
+; args: 16bit number
 %macro rputd 1
 pusha
 mov ax, %1
@@ -13,12 +14,13 @@ mov dx, 0       ; rest
 mov bx, RPUTD_SELECTORS  ; digit selector
 
 rputd_loop:
-div word [bx]          ; div ax bx => ax=digit, dx=rest
-add ax, N_TO_ASCII_DIFF
-mov ah, 0x0E
+div word [bx]           ; div ax bx => ax=digit, dx=rest
+add ax, N_TO_ASCII_DIFF ; convert int to char
+mov ah, 0x0E            ; print config
 int INT_VIDEO
-mov ax, dx
-cmp bx, RPUTD_SELECTORS + 4
+mov ax, dx              ; move rest to ax so it can be reused in the next iteration
+mov dx, 0               ; reset dx
+cmp bx, RPUTD_SELECTORS + 4 ; check if all selectors have been used
 je rputd_end
 inc bx
 jmp rputd_loop
