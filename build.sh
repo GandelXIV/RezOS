f42161bf164ca4dd8933c71dc9712522e01c56dc
GandelXIV <filip@chovanec.com>
2022-06-16 18:10:44 +0200

    Moved kernel into separate directory

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
+cd kernel/
 cargo build --target triple_x86_64.json
-nasm -f elf64 entry.asm -o build/kentry.o 
-ld -T kernel.ld build/kentry.o target/triple_x86_64/debug/libkernel.rlib -o build/kernel.bin
+nasm -f elf64 entry.asm -o ../build/kentry.o 
+ld -T kernel.ld ../build/kentry.o target/triple_x86_64/debug/libkernel.rlib -o ../build/kernel.bin
 
-~/repos/bootboot/mkbootimg/mkbootimg check build/kernel.bin
+~/repos/bootboot/mkbootimg/mkbootimg check ../build/kernel.bin
