58e7e7182f9b15b80d31eee4585c88d046072e03
GandelXIV <filip@chovanec.com>
2022-10-06 20:01:03 +0200

    Fix request conflicts in limine
    Still have to process them though

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,19 +3,24 @@
 #![crate_type = "staticlib"]
 
 // Do not remove this import, it prevents link errors
-use rlibc;
 use core::panic::{self, PanicInfo};
-use limine::LimineTerminalRequest;
-
-static TERMINAL_REQUEST: LimineTerminalRequest = LimineTerminalRequest::new(0);
+#[allow(unused_imports)]
+use rlibc;
 
 #[panic_handler]
 fn kpanic(_pi: &core::panic::PanicInfo<'_>) -> ! {
     loop {}
 }
 
+// TODO: process these requests
+extern "C" {
+    static LIMINE_REQUEST_BOOT_INFO: LimineRequestBootInfo;
+    static LIMINE_REQUEST_TERMINAL: LimineRequestBootInfo;
+}
+
+
+
 #[no_mangle]
 pub extern "C" fn kmain() {
-    let r = TERMINAL_REQUEST.get_response();
     loop {}
 }
