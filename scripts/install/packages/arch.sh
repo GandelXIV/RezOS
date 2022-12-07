ad794840a8e52ddec145f5004ed473b8ec950a27
FIlip Chovanec <filip@chovanec.com>
2022-12-07 10:22:29 +0100

    License scripts

--- scripts/install/packages/arch.sh
+++ scripts/install/packages/arch.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 set -xe
 # see more in scripts/install/packages/packages.md
 pacman -Sy --noconfirm curl make nasm gcc qemu graphviz mtools xorriso automake
