50971e08e8d8ccd22c197354f5515240922d8c8b
FIlip Chovanec <filip@chovanec.com>
2022-12-07 13:11:21 +0100

    License gdt.rs

--- kernel/src/arch/amd64/gdt.rs
+++ kernel/src/arch/amd64/gdt.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 // loads the Global Descriptor Table
 // main source: https://wiki.osdev.org/Global_Descriptor_Table
 
