75c7d20fbb2869f097722afa830f90baa7d20f00
FIlip Chovanec <filip@chovanec.com>
2022-12-07 10:17:39 +0100

    Switch to Mozilla Public License

--- kernel/src/limine.rs
+++ kernel/src/limine.rs
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 // This module handles all things limine
 // See more about the protocol: https://github.com/limine-bootloader/limine/blob/trunk/PROTOCOL.md
 use crate::enum_names;
--- kernel/src/tools.rs
+++ kernel/src/tools.rs
@@ -1,4 +1,8 @@
-// Contains useful macros and utils that can be used anywhere in the kernel
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 
 // implements Into<str> for any empty variant of a public enum
 #[macro_export]
