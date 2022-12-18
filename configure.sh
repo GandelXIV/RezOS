dcd15cd9cfc73c9364bc3449ecd728668b73f3ca
GandelXIV <filip@chovanec.com>
2022-12-18 20:16:21 +0100

    Symlink configure.sh

--- configure.sh
+++ configure.sh
@@ -1,10 +1,1 @@
-#!/bin/bash
-
-set -x
-
-mkdir -p build/
-rm -rf isoroot/ # dir moved to build/
-mkdir -p build/isoroot_x86_64/
-mkdir -p build/isoroot_aarch64/
-mkdir -p log/
-
+scripts/configure.sh
