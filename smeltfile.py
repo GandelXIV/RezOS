69c72622d60b41b2aa31499872599c14f5d8144b
Filip Chovanec <chovafil@fit.cvut.cz>
2024-12-08 16:38:18 +0100

    Working kernel build

--- smeltfile.py
+++ smeltfile.py
@@ -6,6 +6,16 @@ from smelt3 import task, use, File, file_tree, shell, create_setting, sett
 
 create_setting("KERNEL_BUILD_RELEASE", False)
 
+@task("kernel")
+def x86_kernel():
+    output = "build/kernel.x86_64.bin"
+    linker_script = use(File("kernel/link/x86_64.ld"))
+    kernel_obj = use(x86_kernel_object())
+    kentry = use(x86_kentry())
+
+    shell(f"ld -T {linker_script} -o {output} {kentry} {kernel_obj}  ")
+    return File(output)
+
 @task("kentry")
 def x86_kentry():
     out = "build/kentry.x86_64.o"
@@ -26,7 +36,6 @@ def x86_kernel_object():
     KERNEL_BUILD_RELEASE = sett("KERNEL_BUILD_RELEASE")
 
     shell(f"cd kernel/ && cargo build --target triple/x86_64.json --lib {"--release" if KERNEL_BUILD_RELEASE else ""}")
-
-    return File(f"kernel/target/{"release" if KERNEL_BUILD_RELEASE else "debug"}/release/libkernel.a")
+    return File(f"kernel/target/x86_64/{"release" if KERNEL_BUILD_RELEASE else "debug"}/libkernel.a")
 
 smelt3.cli()
