6dbc11d9b33b6c82144eb5d7b997164fb4aa946f
Filip Chovanec <chovafil@fit.cvut.cz>
2024-12-20 16:11:04 +0100

    Optimize static allocation

--- kernel/src/memman/staticalloc.rs
+++ kernel/src/memman/staticalloc.rs
@@ -10,6 +10,7 @@ static GLOBAL_STATIC_ALLOCATOR: StaticAllocator<MAX> = StaticAllocator::new();
 struct StaticAllocator<const SIZE: usize> {
     buffer: [u8; SIZE],
     bump_addr: spin::Mutex<usize>,
+    refcount: spin::Mutex<usize>,
 }
 
 impl<const SIZE: usize> StaticAllocator<SIZE> {
@@ -17,6 +18,8 @@ impl<const SIZE: usize> StaticAllocator<SIZE> {
         Self {
             // just a data holder, not actually referenced but still needed
             buffer: [0; SIZE],
+            // IMPORTANT: If you are going to use this field, consult: https://doc.rust-lang.org/alloc/alloc/trait.GlobalAlloc.html#safety
+            refcount: spin::Mutex::new(0),
             // get properly inited in its `alloc` definition
             bump_addr: spin::Mutex::new(0),
         }
@@ -27,7 +30,6 @@ unsafe impl<const SIZE: usize> GlobalAlloc for StaticAllocator<SIZE> {
     unsafe fn alloc(&self, layout: core::alloc::Layout) -> *mut u8 {
         // expect by rust docs
         if layout.size() == 0 {
-            log!("ZERO!");
             return core::ptr::null_mut();
         }
 
@@ -52,9 +54,25 @@ unsafe impl<const SIZE: usize> GlobalAlloc for StaticAllocator<SIZE> {
         }
         // upadte that ting
         *bumplock = aligned_start + layout.size();
+        *(self.refcount.lock()) += 1;
 
         aligned_start as *mut u8
     }
-    // we dont free this lmao
-    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {}
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: core::alloc::Layout) {
+        let mut refc = self.refcount.lock();
+        let mut bumplock = self.bump_addr.lock();
+
+        // if we are freeing the last allocated object, we can safely move bump backwards, since there are no objects in between.
+        if *bumplock == (ptr as usize) + layout.size() {
+            *bumplock -= layout.size();
+        }
+
+        *refc -= 1;
+
+        // similar to an arena, we reset the whole thing when there are no objects in it.
+        if *refc <= 0 {
+            *bumplock = self.buffer.as_ptr() as usize;
+        }
+    }
 }
